5c23dd3974852fbdd824312c122029f003a06cd7
mplawner <mplawner@gmail.com>
2023-10-27 16:39:50 -0400

    Fixed headlines on Mastodon post to stay within limit

--- mastodon_publisher.py
+++ mastodon_publisher.py
@@ -8,6 +8,16 @@ from config_handler import (
 )
 import time
 
+def truncate_text_to_limit(main_text, limit=1008):
+    """
+    Truncate the text to a specified character limit and append "..." if truncated.
+    """
+    if len(main_text) > limit:
+        truncated_text = main_text[:limit-3] + "..."
+    else:
+        truncated_text = main_text
+    return truncated_text
+
 def post_status_with_audio(audio_path, headlines):
     # Read configuration values from config.txt
     instance_url = read_mastodon_instance_url_from_config()
@@ -25,11 +35,18 @@ def post_status_with_audio(audio_path, headlines):
 
     # Prepare the status text
     hour = datetime.now().strftime('%I %p')  # Gets the current hour in 12-hour format with AM/PM
-    status_text = (f"The {hour} update from Israel Today: Ongoing War Report brought to you by Noa Levi on the ongoing war "
-                   f"between Israel and Hamas following the October 7th massacre of Israeli civilians.\n\n"
-                   f"{headlines}\n\n"
-                   f"Podcast Details: https://www.spreaker.com/show/israel-today-ongoing-war-report \n\n"
-                   f"Listen and Subscribe on your favorite podcast app.\n\n#Israel #Hamas #Gaza #Palestine")
+
+    status_base_text = (f"The {hour} update from Israel Today: Ongoing War Report brought to you by Noa Levi on the ongoing war "
+                        f"between Israel and Hamas following the October 7th massacre of Israeli civilians.\n\n"
+                        f"Podcast Details: https://www.spreaker.com/show/israel-today-ongoing-war-report \n\n"
+                        f"Listen and Subscribe on your favorite podcast app.\n\n#Israel #Hamas #Gaza #Palestine")
+
+    #full_text = status_base_text + '\n' + '\n'.join(headlines)
+    full_text = f"{status_base_text}\n\n{headlines}"
+    print(full_text)    
+
+    status_text = truncate_text_to_limit(full_text)
+    print(status_text)
 
     # Post audio with status
     media = mastodon.media_post(audio_path, mime_type='audio/mpeg')
